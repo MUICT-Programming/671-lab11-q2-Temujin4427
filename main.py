fd6c9f1ac73b081e3744a7ceec2a55281c7ba8de
Temujin4427 <weeraphat.uth@student.mahidol.ac.th>
2024-10-20 21:40:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # YOUR CODE HERE
+n = int(input())
+lst1 = []
+lst2 = []
+
+for i in range(n):
+    lst1.append(int(input()))
+for i in range(n):
+    lst2.append(int(input()))
+
+def summation(lis1,lis2):
+    updated_list = []
+    for i in range(n):
+        updated_list.append(lis1[i] + lis2[i])
+    return updated_list
+
+def find_min_max(lis):
+    return min(lis),max(lis)
+
+sum = summation(lst1,lst2)
+rst = find_min_max(sum)
+
+print(sum)
+print(rst)
